b2de69f666260639ab10dcf6ffaa2ba0da61fd45
fajae88 <167465309+fajae88@users.noreply.github.com>
2024-05-09 15:26:50 -0300

    Gestao de Alunos
    Criamos o banco de dados

--- GestorDeAlunoT6/MeuBancoDeDados.cs
+++ GestorDeAlunoT6/MeuBancoDeDados.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace GestorDeAlunoT6
 {
     internal class MeuBancoDeDados
     {
-
+        private MySqlConnection conexao = new MySqlConnection("datasource=localhost;port=3306;username=root;password=;database=sga_estudantes_bd_t6"); 
     }
 }
